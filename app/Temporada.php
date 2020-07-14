8f7e74e20a72019adf4ee5887c78c2072b8a17a7
Denis F <denisf.salles@gmail.com>
2020-07-13 23:46:44 -0300

    Add temporadas, espisodios

---
+++ app/Temporada.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Temporada extends Model
+{
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class);
+    }
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+}
