b0f2543112f905b4a3eb36ae387c9030882a443f
Denis F <denisf.salles@gmail.com>
2020-07-15 22:55:01 -0300

    Add new temporadas and episodios inputs in create.blade.php

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -20,11 +20,23 @@
         </div>
     @endif
 
-    <form class="container" method="post" action="{{route('criar_serie')}}" >
+    <form class="container" method="post" action="{{route('criar_serie')}}">
         @csrf
-        <div class="form-group">
-            <label for="inputNameSerie">Nome</label>
-            <input type="text" class="form-control" id="inputNameSerie" name="nome">
+        <div class="row">
+            <div class="form-group col-md-8">
+                <label for="inputNameSerie">Nome</label>
+                <input type="text" class="form-control" id="inputNameSerie" name="nome">
+            </div>
+
+            <div class="form-group col-md-2">
+                <label for="numeroTemporadas">N° temporadas</label>
+                <input type="number" class="form-control" id="numeroTemporadas" name="numero_temporadas">
+            </div>
+
+            <div class="form-group col-md-2">
+                <label for="qtdEpisodios">Ep. por temporada</label>
+                <input type="number" class="form-control" id="qtdEpisodios" name="qtd_episodios">
+            </div>
         </div>
         <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
