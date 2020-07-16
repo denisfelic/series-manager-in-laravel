6abce123804c7d47acff763a4b8d3314d8eb7d51
Denis F <denisf.salles@gmail.com>
2020-07-15 22:58:32 -0300

    Add new Helper for dependency injection for create an Serie object and insert in the database

---
+++ app/Services/CriadorSeries.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Serie;
+use Illuminate\Support\Facades\DB;
+
+class CriadorSeries
+{
+
+    public function criarSerie(string $nomeSerie, int $numeroTemporadas, int $quantidadeEpisodios): string
+    {
+
+        DB::beginTransaction();
+        $newSerie = Serie::create([
+            "nome" => $nomeSerie,
+        ]);
+        $this->criaTemporada($numeroTemporadas, $newSerie, $quantidadeEpisodios);
+//        DB::rollBack(); caso der erro, reverte e não aplica.
+        DB::commit();
+        return $nomeSerie;
+
+    }
+
+    /**
+     * @param int $numeroTemporadas
+     * @param $serie
+     * @param int $quantidadeEpisodios
+     */
+    private function criaTemporada(int $numeroTemporadas, $serie, int $quantidadeEpisodios): void
+    {
+        for ($i = 1; $i <= $numeroTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(["numero" => $i]);
+
+            $this->criaEpisodios($quantidadeEpisodios, $temporada);
+        }
+    }
+
+    /**
+     * @param int $quantidadeEpisodios
+     * @param $temporada
+     */
+    private function criaEpisodios(int $quantidadeEpisodios, $temporada): void
+    {
+        for ($j = 1; $j <= $quantidadeEpisodios; $j++) {
+            $temporada->episodios()->create(["numero" => $j]);
+        }
+    }
+}
